9bbe057c5328b1dc94479bff2c0bbd6d729ffae5
Ravendra <164659838+Unos001@users.noreply.github.com>
2026-03-06 10:11:12 +0530

    Made some loading fixes

--- src/prediction.py
+++ src/prediction.py
@@ -1,23 +1,20 @@
 import os
 import joblib
 
-# Load the trained model
+# get current script directory
+BASE_DIR = os.path.dirname(__file__)
 
-try:
-    model_path = os.path.join(os.path.dirname(__file__), "diabetes_model.pkl")
-    model = joblib.load(model_path)
-except FileNotFoundError:
-    print("Model file not found. Make sure diabetes_model.pkl is in the src folder.")
-    exit()
+# model path
+model_path = os.path.join(BASE_DIR, "diabetes_model.pkl")
 
-# Example patient data
-# Order: Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age
-sample = [[2, 120, 70, 20, 79, 25.0, 0.5, 30]]
+# load model
+model = joblib.load(model_path)
 
-# Make prediction
+# example patient data
+sample = [[2,120,70,20,79,25.0,0.5,30]]
+
 prediction = model.predict(sample)
 
-# Display result
 if prediction[0] == 1:
     print("Person is likely Diabetic")
 else:
